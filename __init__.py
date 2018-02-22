e1566f3db4ce892d2342ca6a4360586480bb966a
Theun Kohlbeck <t.kohlbeck@gmail.com>
2018-02-22 18:07:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,20 @@ config = ConfigurationManager.get()
 end = False
 restart = False
 
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+
 class LsttSkill(MycroftSkill):
     def __init__(self):
         super(LsttSkill, self).__init__(name="LsttSkill")
